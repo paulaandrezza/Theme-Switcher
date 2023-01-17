aaca5fcd448f026101b3e406eaf570bf6d5fbd95
paulaandrezza <paulaandrezza25@gmail.com>
2023-01-17 16:34:38 -0300

    change toggle for css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,5 @@
 const button = document.getElementById('button');
-var toggle = true;
 
 button.addEventListener("click", function(){
-    if(toggle){
-        button.style.marginLeft = "110px";
-        document.body.style.backgroundColor = "#292C35";
-        document.body.style.color = "white";
-        toggle = false;
-    } else {
-        button.style.marginLeft = "0px"
-        document.body.style.backgroundColor = "#F1F1F1";
-        document.body.style.color = "black";
-        toggle = true;
-    }
-    console.log(toggle);
+        document.body.classList.toggle('dark')
 });
